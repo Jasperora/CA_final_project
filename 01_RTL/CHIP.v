e8c22dcd81f5eb571937d5d9037c5810a3d55b5e
Hsuan Yi Wu <redpig815@gmail.com>
2023-12-22 16:34:28 +0800

    cache: with error

--- 01_RTL/CHIP.v
+++ 01_RTL/CHIP.v
@@ -20,7 +20,7 @@ module CHIP #(                                                                  
         output              o_finish,                                                           //
     // cache                                                                                    //
         input               i_cache_finish,                                                     //
-        output              o_proc_finish                                                       //
+        output              o_proc_finish                                                       // TODO: call cache to clean all dirty blocks
 );                                                                                              //
 //----------------------------- DO NOT MODIFY THE I/O INTERFACE!! ------------------------------//
 
@@ -150,7 +150,9 @@ module CHIP #(                                                                  
     assign o_DMEM_wdata = DMEM_wdata;   // TODO: o_DMEM_wdata
 
     // finish
-    assign o_finish = finish;
+    assign o_finish = i_cache_finish;
+    assign o_proc_finish = finish;
+    // assign o_finish = finish;
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 // Submoddules
@@ -190,6 +192,7 @@ module CHIP #(                                                                  
 
         case (i_IMEM_data[6:0])
             auipc_opcode: begin
+                $display("auipc");
                 // auipc
                 imm = {i_IMEM_data[31:12], 12'b0};
                 rdatad_nxt = $signed(PC) + $signed(imm);
@@ -339,6 +342,7 @@ module CHIP #(                                                                  
     
 
     always @(posedge i_clk or negedge i_rst_n) begin
+        $display("hello");
         if (!i_rst_n) begin
 
             PC <= 32'h00010000; // Do not modify this value!!!
@@ -366,8 +370,7 @@ module CHIP #(                                                                  
     end
 endmodule
 
-module Reg_file(i_clk, i_rst_n, wen, rs1, rs2, rd, wdata, rdata1, rdata2);
-   
+module Reg_file(i_clk, i_rst_n, wen, rs1, rs2, rd, wdata, rdata1, rdata2);  
     parameter BITS = 32;
     parameter word_depth = 32;
     parameter addr_width = 5; // 2^addr_width >= word_depth
@@ -423,13 +426,13 @@ module MULDIV_unit#(
 
         output [BIT_W - 1 : 0]      o_data,  // output value
         output                      o_done   // output valid signal
-    );
-     // Do not Modify the above part !!!
-// Parameters
+);
+    // Do not Modify the above part !!!
+    // Parameters
     parameter S_IDLE           = 2'd0;
     parameter S_MULTI_CYCLE_OP = 2'd2;
 
-// Wires & Regs
+    // Wires & Regs
     // state
     reg  [           1: 0] state, state_nxt; // remember to expand the bit width if you want to add more states!
     // load input
@@ -437,14 +440,14 @@ module MULDIV_unit#(
     reg  [     BIT_W-1: 0] operand_b, operand_b_nxt;
     reg  [           2: 0] inst, inst_nxt;
 
-// Wire Assignments
+    // Wire Assignments
     // Counter
     reg  [4:0] cnt, cnt_nxt;
     // Output
     reg  [2*BIT_W - 1 : 0] o_data_cur, o_data_nxt;
     reg                     o_done_cur, o_done_nxt;
     
-// Always Combination
+    // Always Combination
     // load input
     always @(*) begin
         if (i_valid) begin
@@ -575,193 +578,279 @@ module Cache#(
         // others
         input  [ADDR_W-1: 0] i_offset
     );
-    assign o_cache_available = 0; // change this value to 1 if the cache is implemented
+    assign o_cache_available = 1; // change this value to 1 if the cache is implemented
 
     //------------------------------------------//
     //          default connection              //
-    assign o_mem_cen = i_proc_cen;              //
-    assign o_mem_wen = i_proc_wen;              //
-    assign o_mem_addr = i_proc_addr;            //
-    assign o_mem_wdata = i_proc_wdata;          //
-    assign o_proc_rdata = i_mem_rdata[0+:BIT_W];//
-    assign o_proc_stall = i_mem_stall;          //
+    // assign o_mem_cen = i_proc_cen;              //
+    // assign o_mem_wen = i_proc_wen;              //
+    // assign o_mem_addr = i_proc_addr;            //
+    // assign o_mem_wdata = i_proc_wdata;          //
+    // assign o_proc_rdata = i_mem_rdata[0+:BIT_W];//
+    // assign o_proc_stall = i_mem_stall;          //
     //------------------------------------------//
 
-    // TODO: BONUS
-    // reg [2:0] state, state_nxt;
+    // registers
+    reg [2:0] state, state_nxt;
+    reg [1:0] mode, mode_nxt;
     
-    // // output registers
-    // reg mem_cen, mem_cen_nxt;
-    // reg mem_wen, mem_wen_nxt;
-    // reg [ADDR_W-1:0] mem_addr, mem_addr_nxt;
-    // reg [BIT_W-1:0] proc_rdata, proc_rdata_nxt;
-    // reg [BIT_W-1:0] proc_wdata, proc_wdata_nxt;
-    // reg [BIT_W*4-1:0] mem_wdata, mem_wdata_nxt;
-    // reg proc_stall, proc_stall_nxt;
+    // data/address
+    reg [ADDR_W-1:0] addr, addr_nxt;
+    reg [BIT_W-1:0] rdata, rdata_nxt;
+    reg [BIT_W-1:0]  wdata, wdata_nxt;
+    reg [ADDR_W-1:0] mem_addr, mem_addr_nxt;
+    reg [BIT_W*4-1:0] mem_rdata, mem_rdata_nxt;
+    reg [BIT_W*4-1:0]  mem_wdata, mem_wdata_nxt;
+    // control signals
+    reg stall, stall_nxt;
+    reg finish, finish_nxt;
+    reg cen, cen_nxt;
+    reg wen, wen_nxt;
 
-    // assign o_mem_cen = mem_cen;              
-    // assign o_mem_wen = mem_wen;              
-    // assign o_mem_addr = mem_addr;            
-    // assign o_mem_wdata = mem_wdata;          
-    // assign o_proc_rdata = proc_rdata;
-    // assign o_proc_stall = proc_stall;          
+    // processor
+    assign o_proc_rdata = rdata;
+    assign o_proc_stall = stall;
+    assign o_cache_finish = finish;
+    // memory
+    assign o_mem_cen = cen;
+    assign o_mem_wen = wen;
+    assign o_mem_addr = mem_addr;
+    assign o_mem_wdata = mem_wdata;
 
+    // parameters
+    // state
+    parameter S_IDLE    = 3'd0;
+    parameter S_WRITE   = 3'd1;
+    parameter S_WB      = 3'd2;
+    parameter S_ALLO    = 3'd3;
+    parameter S_READ    = 3'd4;
+    parameter S_CLEAN   = 3'd5; // write baack all dirty blocks when i_proc_finish=1
+    // mode
+    parameter M_READ    = 2'd0;
+    parameter M_WRITE   = 2'd1;
+    parameter M_CLEAN   = 2'd2;
 
-    // // entries
-    // /*
-    // [tag |index|block offset|byte offset]
-    // [31:8|7:4|3:2|1:0]
-    // [24| 4| 2| 2]
-    // 16 blocks
-    // */
-    // reg v [0:15], v_nxt [0:15];
-    // reg dirty [0:15], dirty_nxt[1:15];
-    // reg [31:8] tag [0:15], tag_nxt [0:15];
-    // reg [BIT_W*4-1:0] entry [0:15], entry_nxt [0:15];
-    // reg write, write_nxt;
+    // entries
+    reg [0:15] v, v_nxt; // valid bit
+    reg [0:15] dirty, dirty_nxt;  // dirty bit
+    reg [31:8] tag [0:15], tag_nxt [0:15];
+    reg [BIT_W*4-1:0] entry [0:15], entry_nxt [0:15];   // 4 entries/block
 
-    // // state
-    // parameter S_IDLE    = 3'd0;
-    // parameter S_WRITE   = 3'd1;
-    // parameter S_WB      = 3'd2;
-    // parameter S_ALLO    = 3'd3;
-    // parameter S_READ    = 3'd4;
+    // FSM
+    reg [4:0] i;
+    always @(*) begin
+        state_nxt = state;
+        mode_nxt = mode;
+        addr_nxt = addr;
+        rdata_nxt = rdata;
+        wdata_nxt = wdata;
+        stall_nxt = stall;
+        finish_nxt = finish;
+        cen_nxt = cen;
+        wen_nxt = wen;
 
-    // // FSM
-    // reg hit; // TODO: assign hit
-    // always @(*) begin
-    //     hit = 0;
+        for (i=0; i<16; i=i+1) begin
+            v_nxt[i] = v[i];
+            dirty_nxt[i] = dirty[i];
+            tag_nxt[i] = tag[i];
+            entry_nxt[i] = entry[i];
+        end
 
-    //     state_nxt = state;
-    //     mem_cen_nxt = mem_cen;
-    //     mem_wen_nxt = mem_wen;
-    //     mem_addr_nxt = mem_addr;
-    //     mem_wdata_nxt = mem_wdatal;
-    //     write_nxt=write;
+        case(state)
+            S_IDLE: begin
+                // $display("S_IDLE");
+                if (i_proc_finish) begin    // write back all dirty blocks
+                    state_nxt = S_CLEAN;
+                    stall_nxt = 1;  // stall
+                end 
+                else begin
+                    if (i_proc_cen) begin
+                        addr_nxt = i_proc_addr;
+                        if (i_proc_wen) begin
+                            state_nxt = S_WRITE;
+                            wdata_nxt = i_proc_wdata;
+                            mode_nxt = M_WRITE;
+                        end
+                        else begin
+                            state_nxt = S_READ;
+                            mode_nxt = M_READ;
+                        end
+                        stall_nxt = 1;  // stall
+                    end
+                    else begin
+                        state_nxt = S_IDLE; // hold
+                        stall_nxt = 0;
+                    end
+                end
+            end
 
-    //     case(state)
-    //         S_IDLE: begin
-    //             write_nxt = 0;
+            S_WRITE: begin
+                // $display("S_WRITE");
+                if (v[addr[7:4]] && (tag[addr[7:4]] == addr[31:8])) begin   // hit
+                    for (i=0; i<16; i=i+1) begin
+                        if (i==addr[7:4]) begin
+                            entry_nxt[i][addr[3:2]<<<5+:BIT_W] = wdata;
+                        end
+                        else begin
+                            entry_nxt[i][addr[3:2]<<<5+:BIT_W] = entry[i][addr[3:2]<<<5+:BIT_W];
+                        end
+                    end
+                    state_nxt = S_IDLE;
+                    stall_nxt = 0;
+                end 
+                else begin  // miss
+                    mem_addr_nxt = {addr[31:4], 4'b0};
 
-    //             if (i_proc_cen) begin
-    //                 // mem_cen_nxt = 1;
-    //                 proc_stall_nxt = 1;
-    //                 mem_addr_nxt = i_proc_addr;
+                    if (dirty[addr[7:4]]) begin // dirty
+                        state_nxt = S_WB;
+                        cen_nxt = 1;
+                        wen_nxt = 1;
+                        mem_wdata_nxt = entry[addr[7:4]];
+                    end
+                    else begin  // !dirty
+                        state_nxt = S_ALLO;
+                        cen_nxt = 1;
+                        wen_nxt = 0;
+                    end
+                end
+            end
 
-    //                 if (i_proc_wen) begin
-    //                     // mem_wen_nxt = 1;
-    //                     proc_wdata_nxt = i_proc_wdata;
-    //                     write_nxt = 1;
-                        
-    //                     state_nxt = S_WRITE;
-    //                 end
-    //                 else begin
-    //                     // mem_wen_nxt = 0;
-    //                     write_nxt = 0;
+            S_WB: begin
+                // $display("S_WB");
+                if (!i_mem_stall) begin
+                    state_nxt =  (mode==M_CLEAN)?S_CLEAN:S_ALLO;
+                    cen_nxt = 1;
+                    wen_nxt = 1;
+                    dirty_nxt[addr[7:4]] = 0;
+                end
+                else begin
+                    state_nxt = S_WB;
+                    cen_nxt = 0;
+                    wen_nxt = 0;
+                end
+            end
 
-    //                     state_nxt = S_READ;
-    //                 end
-    //             end
-    //             else begin
-    //                 state_nxt = state;
-    //             end
-    //         end
-    //         S_WRITE: begin
-    //             integer i;
-    //             for (i = 0; i < 4; i = i + 1) begin
-    //                 entry_nxt[mem_addr[7:4]][i*32+:32] = (i == i_proc_addr[3:2])? proc_rdata: entry[mem_addr[7:4]][i*32+:32];
-    //             end
+            S_ALLO: begin
+                // $display("S_ALLO");
+                if (!i_mem_stall) begin
+                    state_nxt  = (mode == M_READ)?S_READ:S_WRITE;
+                    cen_nxt = 0;
+                    wen_nxt = 0;
+                    for (i=0; i<16; i=i+1) begin
+                        if (i==mem_addr[7:4]) begin
+                            entry_nxt[i] = i_mem_rdata;
+                            tag_nxt[i] = mem_addr[31:8];
+                            v_nxt[i] = 1;
+                        end
+                        else begin
+                            entry_nxt[i] = entry[i];
+                            tag_nxt[i] = tag[i];
+                            v_nxt[i] = v[i];
+                        end
+                    end
+                end
+                else begin
+                    state_nxt = S_ALLO;
+                    cen_nxt = 0;
+                    wen_nxt = 0;
+                end
+            end
 
-    //             if v[i_proc_addr[7:4]] && (i_proc_addr[31:8] == tag[31:8]) begin  // valid and hit
-    //                 dirty_nxt[mem_addr[7:4]] = 1;
-    //                 state_nxt = S_IDLE;
-    //             end
-    //             else begin  // miss
-    //                 mem_wdata_nxt = entry[mem_addr[7:4]];  // write replaced block back
-                    
-    //             end
+            S_READ: begin
+                // $display("S_READ");
+                if (v[addr[7:4]] && (tag[addr[7:4]] == addr[31:8])) begin   // hit
+                    rdata_nxt = entry[addr[7:4]][addr[3:2]<<<5+:BIT_W];
+                    state_nxt = S_IDLE;
+                    stall_nxt = 0;
+                end 
+                else begin  // miss
+                    mem_addr_nxt = {addr[31:4], 4'b0};
 
-    //             if (dirty) begin
-    //                 state_nxt = S_WB;
-    //             end
-    //             else begin 
-    //                 state_nxt = S_IDLE;
-    //             end
-    //         end
-    //         S_WB: begin 
-    //             if (!i_mem_stall) begin
-    //                 if (i_proc_wen) begin   // REVIEW: p57 i_proc_write?
-    //                     state_nxt = S_WRITE;
-    //                 end
-    //                 else begin
-    //                     state_nxt = S_ALLO;
-    //                 end
-    //             end
-    //             else begin
-    //                 state_nxt = state;
-    //             end
-    //         end
-    //         S_ALLO: begin
-    //             if (!i_mem_stall) begin
-    //                 state_nxt = S_READ;
-    //             end
-    //             else begin
-    //                 state_nxt = state;
-    //             end
-    //         end
-    //         S_READ: begin
-    //             if (dirty) begin
-    //                 state_nxt = S_WB;
-    //             end else begin
-    //                 if (dirty) begin
-    //                     state_nxt = S_WB;
-    //                 end
-    //                 else begin
-    //                     if (hit) begin
-    //                         state_nxt = S_IDLE;
-    //                     end
-    //                     else begin
-    //                         state_nxt = S_ALLO;
-    //                     end
-    //                 end
-    //             end           
-    //         end
-    //         default: begin
-    //         end
-    //     endcase
-    // end
+                    if (dirty[addr[7:4]]) begin // dirty
+                        state_nxt = S_WB;
+                        cen_nxt = 1;
+                        wen_nxt = 1;
+                        mem_wdata_nxt = entry[addr[7:4]];
+                    end
+                    else begin  // !dirty
+                        state_nxt = S_ALLO;
+                        cen_nxt = 1;
+                        wen_nxt = 0;
+                    end
+                end
+            end
 
-    // always @(posedge i_clk or negedge i_rst_n) begin
-    //     if (!i_rst_n) begin
-    //         state <= S_IDLE;
-    //         v <= '{default:1'b0};
-    //         dirty <= '{default:1'b0};
-    //         tag <= '{default:24'b0};
-    //         entry <= '{default:31'b0};
-    //         write <= 0;
-            
-    //         mem_wen <= 0;
-    //         mem_cen <= 0;
-    //         mem_addr <= 0;
-    //         mem_wdata <= 0;
-    //         proc_rdata <= 0;
-    //         proc_stall <= 0;
-    //     end
-    //     else begin
-    //         state <= state_nxt;
-    //         v = v_nxt;
-    //         dirty <= dirty_nxt;
-    //         tag <= tag_nxt;
-    //         entry <= entry_nxt;
-    //         write <= write_nxt;
+            S_CLEAN : begin
+                // $display("S_CLEAN");
+                if (dirty == 0) begin
+                    state_nxt = S_IDLE;
+                    finish_nxt = 1;
+                    stall_nxt = 0;
+                end
+                else begin
+                    state_nxt = S_WB;
+                    for (i=0; i<16; i=i+1) begin
+                        if (dirty[i]) begin
+                            mem_addr_nxt = {tag[i], i[3:0], 4'b0};
+                        end
+                    end
+                    cen_nxt = 0;
+                    wen_nxt = 1;
+                    mem_wdata_nxt = entry[mem_addr_nxt];
+                end
+            end
 
-    //         mem_wen <= mem_wen_nxt;
-    //         mem_cen <= mem_cen_nxt;
-    //         mem_addr <= mem_addr_nxt;
-    //         mem_wdata <= mem_wdata_nxt;
-    //         proc_rdata <= proc_rdata_nxt;
-    //         proc_stall <= proc_stall_nxt;
-    //     end
-    // end
+            default: begin
+                // $display("error");
+            end
+        endcase
+    end
 
+    always @(posedge i_clk or negedge i_rst_n) begin
+        // $display("Hi");
+        // $display("%d", i_rst_n);
+        // $display("%d", i_clk);
+        if (!i_rst_n) begin
+            state <= S_IDLE;
+            mode <= M_READ;
+            addr <= 0;
+            rdata <= 0;
+            wdata <= 0;
+            mem_rdata <= 0;
+            mem_wdata <= 0;
+            mem_addr <= 0;
+            stall <= 0;
+            finish <= 0;
+            cen <= 0;
+            wen <= 0;
+
+            for (i=0; i<16; i=i+1) begin
+                v[i] <= 0;
+                dirty[i] <= 0;
+                tag[i] <= 0;
+                entry [i] <= 0;
+            end
+        end
+        else begin
+            state <= state_nxt;
+            mode <= mode_nxt;
+            addr <= addr_nxt;
+            rdata <= rdata_nxt;
+            wdata <= wdata_nxt;
+            mem_rdata <= mem_rdata_nxt;
+            mem_wdata <= mem_wdata_nxt;
+            mem_addr <= mem_addr_nxt;
+            stall <= stall_nxt;
+            finish <= finish_nxt;
+            cen <= cen_nxt;
+            wen <= wen_nxt;
+
+            for (i=0; i<16; i=i+1) begin
+                v[i] <= v_nxt[i];
+                dirty[i] <= dirty_nxt[i];
+                tag[i] <= tag_nxt[i];
+                entry[i] <= entry_nxt[i];
+            end
+        end
+    end
 endmodule
